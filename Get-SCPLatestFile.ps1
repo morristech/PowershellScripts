41239361f0e4afcc48d8eba9d5cc5c5a0b3db94d
FingersOnFire <fingersonfirecom+github@gmail.com>
2017-06-01 14:55:01 +0200

    Added SCP copy scripts using WinSCP

---
+++ Get-SCPLatestFile.ps1
@@ -0,0 +1,62 @@
+﻿param (
+    $localPath = "c:\Temp\",
+    $remotePath = "/home/pi/"
+)
+ 
+try
+{
+    # Load WinSCP .NET assembly
+    Add-Type -Path "lib\WinSCP\WinSCPnet.dll"
+ 
+    # Setup session options
+    $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
+        #Protocol = [WinSCP.Protocol]::Sftp
+        Protocol = [WinSCP.Protocol]::Scp 
+        HostName = ""
+        UserName = ""
+        Password = ""
+        GiveUpSecurityAndAcceptAnySshHostKey = "true" 
+        #SshHostKeyFingerprint = "*"
+        #SshHostKeyFingerprint = "ssh-rsa 2048 xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx"
+    }
+ 
+    $session = New-Object WinSCP.Session
+ 
+    try
+    {
+        # Connect
+        $session.Open($sessionOptions)
+ 
+        # Get list of files in the directory
+        $directoryInfo = $session.ListDirectory($remotePath)
+ 
+        # Select the most recent file
+        $latest =
+            $directoryInfo.Files |
+            Where-Object { -Not $_.IsDirectory } |
+            Sort-Object LastWriteTime -Descending |
+            Select-Object -First 1
+ 
+        # Any file at all?
+        if ($latest -eq $Null)
+        {
+            Write-Host "No file found"
+            exit 1
+        }
+ 
+        # Download the selected file
+        $session.GetFiles($session.EscapeFileMask($remotePath + $latest.Name), $localPath).Check()
+    }
+    finally
+    {
+        # Disconnect, clean up
+        $session.Dispose()
+    }
+ 
+    exit 0
+}
+catch [Exception]
+{
+    Write-Host ("Error: {0}" -f $_.Exception.Message)
+    exit 1
+}
---
+++ Sync-SCPFilesToLocal.ps1
@@ -0,0 +1,54 @@
+﻿param (
+    $localPath = "c:\Temp\",
+    $remotePath = "/home/user/",
+    $mask = "*.*"
+)
+ 
+try
+{
+    # Load WinSCP .NET assembly
+    Add-Type -Path "lib\WinSCP\WinSCPnet.dll"
+ 
+    # Setup session options
+    $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
+        #Protocol = [WinSCP.Protocol]::Sftp
+        Protocol = [WinSCP.Protocol]::Scp 
+        HostName = ""
+        UserName = ""
+        Password = ""
+        GiveUpSecurityAndAcceptAnySshHostKey = "true" 
+        #SshHostKeyFingerprint = "*"
+        #SshHostKeyFingerprint = "ssh-rsa 2048 xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx"
+    }
+ 
+    $session = New-Object WinSCP.Session
+ 
+    try
+    {
+        # Connect
+        $session.Open($sessionOptions)
+ 
+        $files = $session.EnumerateRemoteFiles($remotePath, $mask, [WinSCP.EnumerationOptions]::None)
+
+        foreach ($fileInfo in $files)
+        {
+            if(![System.IO.File]::Exists($localPath + $fileInfo.Name)){
+                Write-Host ("Downloading {0} ..." -f $fileInfo.FullName)
+                $session.GetFiles($session.EscapeFileMask($fileInfo.FullName), $localPath + "\*").Check() 
+            }
+            
+        }
+    }
+    finally
+    {
+        # Disconnect, clean up
+        $session.Dispose()
+    }
+ 
+    exit 0
+}
+catch [Exception]
+{
+    Write-Host ("Error: {0}" -f $_.Exception.Message)
+    exit 1
+}
