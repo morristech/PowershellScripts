671eeed523fc03cceb175056e7f57dc57f316a5c
Leonard Beaud <leonard@isystems.local>
2017-10-31 16:02:09 +0100

    Change Mso EchangeUrl to the default one

--- Connect-MSOLandExchangeOnline.ps1
+++ Connect-MSOLandExchangeOnline.ps1
@@ -4,9 +4,9 @@ $Credentials = Get-Credential -Credential $UserAdmin
 
 ## Connection into Office 365 Management
 Connect-MsolService -Credential $Credentials
-$MsoExchangeURL = "https://ps.outlook.com/PowerShell-LiveID?PSVersion=5.0.10586.122"
 
 ## Connection into Exchange Management
+$MsoExchangeURL = "https://outlook.office365.com/powershell-liveid/"
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $MsoExchangeURL -Credential $Credentials -Authentication Basic -AllowRedirection
 
 ## Import session allowing to override current commands
