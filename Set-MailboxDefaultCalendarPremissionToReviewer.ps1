5d723c499b2fb1862fb6cba3d159646f0e1ea00d
Leonard Beaud <leonard@isystems.local>
2017-10-31 15:12:04 +0100

    Simplified script for now. Does not handle exeptions yet

--- Set-MailboxDefaultCalendarPremissionToReviewer.ps1
+++ Set-MailboxDefaultCalendarPremissionToReviewer.ps1
@@ -1,7 +1,2 @@
 $mailbox = Read-Host 'Mailbox Name ?' 
-if(Get-Mailbox -Identity $mailbox) {
-    Set-MailboxFolderPermission -Identity '$mailbox:\Calendar' -User "Default" -AccessRights "Reviewer"
-}
-else {
-    Write-Host 'No mailbox with such name : $mailbox'
-}
+Set-MailboxFolderPermission -Identity '$mailbox:\Calendar' -User "Default" -AccessRights "Reviewer"
