a458e9777e3ea06e09716d519da06d91418bc338
Leonard Beaud <leonard@isystems.local>
2018-05-24 12:03:38 +0200

    Added ACL Report Script for direct subfolders

---
+++ Get-SubFoldersACL.ps1
@@ -0,0 +1,32 @@
+﻿param (
+    [string]$FolderPath = $( Read-Host "Folder Path" )
+)
+
+$FolderList = Get-ChildItem $FolderPath 
+$ExcludedUsers = @("NT AUTHORITY\SYSTEM", "NT AUTHORITY\SYSTEM", "CREATOR OWNER", "BUILTIN\Administrators")
+$ACLReport = @();
+
+foreach ($Folder in $FolderList){
+
+    $FolderACLList = Get-Acl $Folder.FullName | Select -expand Access
+
+    foreach ($FolderACL in $FolderACLList){
+    
+        if($ExcludedUsers -notcontains $FolderACL.IdentityReference){
+    
+           $ACLReportData = @{
+                "FolderName" = $Folder.Name;
+                "FolderFullName" = $Folder.FullName;
+                "Identity" = $FolderACL.IdentityReference;
+                "AccessRights" = $FolderACL.FileSystemRights;
+           }
+
+           $ACLReport += (New-Object -TypeName PSObject -Property $ACLReportData)
+
+        }
+    
+    }
+
+}
+
+$ACLReport
