9a72d55f551ea24692e3e4c35c6b1524057f54c9
Leonard Beaud <leonard@isystems.local>
2018-10-26 18:09:38 +0200

    Added a script that recreate the default OWA site and reset IIS

---
+++ Recreate-OWAVirtualDirectory.ps1
@@ -0,0 +1,8 @@
+﻿
+$hostname = $(hostname)
+
+Remove-OwaVirtualDirectory -Identity "$hostname\owa (Default Web Site)"
+New-OwaVirtualDirectory -WebSiteName "Default Web Site"
+Set-owavirtualdirectory -identity "$hostname\owa (Default Web Site)" -AdfsAuthentication $False -BasicAuthentication $true -WindowsAuthentication $false -DigestAuthentication $false -FormsAuthentication $true -LogonFormat PrincipalName
+
+IISRESET /noforce
