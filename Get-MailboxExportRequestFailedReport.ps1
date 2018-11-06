1239dadce15d24def0ca43c5ca970d2b2f1e03a6
Leonard Beaud <leonard@isystems.local>
2018-11-06 14:25:28 +0100

    Add Get-MailboxExportRequestFailedReport.ps1
    This script create a log file containing the status of all failed mailbox export request.

---
+++ Get-MailboxExportRequestFailedReport.ps1
@@ -0,0 +1,5 @@
+﻿param (
+    [string]$ExportPath = "C:\Temp"
+)
+
+ Get-MailboxExportRequest -status failed | Get-MailboxExportRequestStatistics -IncludeReport | Format-List > $ExportPath + "\report.txt"
