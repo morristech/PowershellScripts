55c0b8876388f26ddee6a7362addaea77e8db5d6
Leonard Beaud <leonard@isystems.local>
2018-11-06 14:23:18 +0100

    Update Set-AllMailboxesDefaultCalendarPremissionToReviewer.ps1
    Retreive main calendar name as it depends on the Mailbox language

--- Set-AllMailboxesDefaultCalendarPremissionToReviewer.ps1
+++ Set-AllMailboxesDefaultCalendarPremissionToReviewer.ps1
@@ -1,7 +1,11 @@
 $Mailboxes = Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox
 
 foreach($Mailbox in $Mailboxes) {
-	$identity = $Mailbox.Name + ":\Calendar"
+
+    $calendars = Get-MailboxFolderStatistics $Mailbox.UserPrincipalName -FolderScope Calendar
+
+    $identity = $Mailbox.UserPrincipalName + ":\" + $calendars[0].Name
+
 	write-host $identity
 	Set-MailboxFolderPermission -Identity $identity -User "Default" -AccessRights "Reviewer"
 }
